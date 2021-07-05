dd7edcaab4b282420861def6782d57d878bfc8a2
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-07-05 16:21:08 -0500

    Se añadió el apartado de SharedPreferences de tal forma que quedó completa la app

--- app/src/main/java/mx/udg/alumnos/sr_persistencia_datos/MainActivity.kt
+++ app/src/main/java/mx/udg/alumnos/sr_persistencia_datos/MainActivity.kt
@@ -20,15 +20,18 @@ class MainActivity : AppCompatActivity() {
     var btnLeer:Button?=null
     var tvMostrar:TextView?=null
 
+    /* SharedPreferences */
+    var btnLeerPref:Button?=null
+    var btnGuardarPref:Button?=null
+    var etClave:EditText?=null
+    var etValor:EditText?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnGuardar = findViewById(R.id.BtnGuardar)
-        etInfo = findViewById(R.id.etInfo)
-        btnLeer = findViewById(R.id.btnLeer)
-        tvMostrar = findViewById(R.id.tvMostrar)
+        initUI()
 
         btnGuardar!!.setOnClickListener {
 
@@ -42,6 +45,25 @@ class MainActivity : AppCompatActivity() {
         btnLeer!!.setOnClickListener {
 
             tvMostrar!!.text = leerArchivo(nombreArchivo)
+        }
+
+        /* SharedPreferences */
+
+        btnLeerPref!!.setOnClickListener {
+
+            val claveABuscar = etClave!!.text.toString()
+            val datoEncotrado = getDatos(claveABuscar)
+
+            etValor!!.setText(datoEncotrado)
+
+        }
+
+        btnGuardarPref!!.setOnClickListener {
+
+            val claveAGuardar = etClave!!.text.toString()
+            val valorABuscar = etValor!!.text.toString()
+            setDatos(claveAGuardar, valorABuscar)
+
         }
 
     }
@@ -68,6 +90,40 @@ class MainActivity : AppCompatActivity() {
         val strinResultado = inputStream.bufferedReader().use { it.readLine() }
         Log.d("Archivos: ", strinResultado)
         return strinResultado
+
+    }
+
+    /*
+    * Metodo para sharedPreferences
+    *
+    * */
+
+    fun setDatos(clave:String, valor:String){
+
+        val prefs = getPreferences(MODE_PRIVATE)
+        prefs.edit().putString(clave, valor).apply()
+    }
+
+    fun getDatos(clave:String):String{
+
+        val prefs = getPreferences(MODE_PRIVATE)
+        val dato = prefs.getString(clave, "No se encontro: ${clave}")
+
+        return dato.toString()
+    }
+
+    fun initUI(){
+
+        btnGuardar = findViewById(R.id.BtnGuardar)
+        etInfo = findViewById(R.id.etInfo)
+        btnLeer = findViewById(R.id.btnLeer)
+        tvMostrar = findViewById(R.id.tvMostrar)
+
+        /* SharedPreferences */
+        btnLeerPref = findViewById(R.id.btnLeerPrefs)
+        btnGuardarPref = findViewById(R.id.btnGuardarPrefs)
+        etClave = findViewById(R.id.etClave)
+        etValor = findViewById(R.id.etValor)
 
     }
 }
